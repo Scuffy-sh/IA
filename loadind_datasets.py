d3bb1d9dc93a10668aa9d8176e1d6ad111995e08
Scuffy-sh <alexpasi.pasalamar@gmail.com>
2025-09-09 11:37:23 +0200

    Delete loadind_datasets.py

--- loadind_datasets.py
+++
@@ -1,6 +0,0 @@
-#Para importar la libreria donde podremos descargar los datasets
-from datasets import load_dataset
-
-#Para cargar un dataset concreto
-dataset = load_dataset("hf-internal-testing/cats_vs_dogs_sample")
-print(dataset)
